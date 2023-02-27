482a0722ed2f18c8fd4b1511ff528174c8fed2a3
ibiri <125995852+faridibiri@users.noreply.github.com>
2023-02-27 10:59:41 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 public static void main(String [] args){
 
-System.out.println("hello trigger faridddddddddddddddxxxxxxxxxxx");
+System.out.println("hello trigger farid update");
 
 }
 }
